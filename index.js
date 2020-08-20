2ef9db89b9ccba6595447fe4677453acefcea791
Daniel <daniel.artuso1@gmail.com>
2020-08-20 17:49:15 -0600

    updated js for react

--- index.js
+++ index.js
@@ -1,3 +1,100 @@
+let picks = {
+    EastQuarter1: "", EastQuarter2: "", EastQuarter3: "", EastQuarter4: "",
+    EastSemi1: "", EastSemi2: "",
+    FinalEast: "",
+    Winner: "",
+    FinalWest: "",
+    WestSemi1: "", WestSemi2: "",
+    WestQuarter1: "", WestQuarter2: "", WestQuarter3: "", WestQuarter4: "",
+}
+
+const seeds = {
+    east: [
+        {
+            name: "Bucks",
+            image: "assets/teams/east/1MIL.svg",
+            seed: 1
+        },
+        {
+            name: "Magic",
+            image: "assets/teams/east/8ORL.svg",
+            seed: 8
+        },
+        {
+            name: "Pacers",
+            image: "assets/teams/east/4IND.svg",
+            seed: 4
+        },
+        {
+            name: "Heat",
+            image: "assets/teams/east/5MIA.svg",
+            seed: 5
+        },
+        {
+            name: "Celtics",
+            image: "assets/teams/east/3BOS.svg",
+            seed: 3
+        },
+        {
+            name: "76ers",
+            image: "assets/teams/east/6PHI.svg",
+            seed: 6
+        },
+        {
+            name: "Raptors",
+            image: "assets/teams/east/2TOR.svg",
+            seed: 2
+        },
+        {
+            name: "Nets",
+            image: "assets/teams/east/7BKN.svg",
+            seed: 7
+        }
+    ],
+    west: [
+        {
+            name: "Lakers",
+            image: "assets/teams/west/1LAL.svg",
+            seed: 1
+        },
+        {
+            name: "Trailblazers",
+            image: "assets/teams/west/8POR.svg",
+            seed: 8
+        },
+        {
+            name: "Houston",
+            image: "assets/teams/west/4HOU.svg",
+            seed: 4
+        },
+        {
+            name: "Thunder",
+            image: "assets/teams/west/5OKC.svg",
+            seed: 5
+        },
+        {
+            name: "Nuggets",
+            image: "assets/teams/west/3DEN.svg",
+            seed: 3
+        },
+        {
+            name: "Jazz",
+            image: "assets/teams/west/6UTA.svg",
+            seed: 6
+        },
+        {
+            name: "Clippers",
+            image: "assets/teams/west/2LAC.svg",
+            seed: 2
+        },
+        {
+            name: "Mavericks",
+            image: "assets/teams/west/7DAL.svg",
+            seed: 7
+        },
+    ]
+}
+
 function image(img, location) {
     const forward = document.getElementById(location)
     let ES1 = document.getElementById('es1').src;
@@ -13,16 +110,19 @@ function image(img, location) {
         case 'eq2':
             if (forward.src === ES1) {
                 document.getElementById('es1').src = "";
+                document.getElementById('es1').alt = "";
             }
         case 'es1':
             if (forward.src === NFE) {
                 document.getElementById('nfe').src = "";
+                document.getElementById('nfe').alt = "";
             }
             break;
         case 'eq3':
         case 'eq4':
             if (forward.src === ES2) {
                 document.getElementById('es2').src = "";
+
             }
         case 'es2':
             if (forward.src === NFE) {
@@ -54,9 +154,10 @@ function image(img, location) {
     }
 
     document.getElementById(location).src = img.src
+    document.getElementById(location).alt = img.alt
 }
 
-function color(location) {
+function winner(location) {
     if (location === 'nfw') {
         document.getElementById('nfw').style.backgroundColor = "#ffd700"
         document.getElementById('nfe').style.backgroundColor = ""
@@ -64,6 +165,7 @@ function color(location) {
         document.getElementById('nfe').style.backgroundColor = "#ffd700"
         document.getElementById('nfw').style.backgroundColor = ""
     }
+    loadBracket(location)
 }
 
 function reset() {
@@ -73,4 +175,26 @@ function reset() {
             document.getElementById(id).src = ""
         }
     )
+}
+
+function loadBracket(winner) {
+    document.getElementById(winner).alt = picks.Winner
+    document.getElementById('nfe').alt = picks.FinalEast
+    document.getElementById('nfw').alt = picks.FinalWest
+
+    document.getElementById('es1').alt = picks.EastSemi1
+    document.getElementById('es2').alt = picks.EastSemi2
+
+    document.getElementById('ws1').alt = picks.WestSemi1
+    document.getElementById('ws2').alt = picks.WestSemi2
+
+    document.getElementById('wq1').alt = picks.WestQuarter1
+    document.getElementById('wq2').alt = picks.WestQuarter2
+    document.getElementById('wq3').alt = picks.WestQuarter3
+    document.getElementById('wq4').alt = picks.WestQuarter4
+
+    document.getElementById('eq1').alt = picks.EastQuarter1
+    document.getElementById('eq2').alt = picks.EastQuarter2
+    document.getElementById('eq3').alt = picks.EastQuarter3
+    document.getElementById('eq4').alt = picks.EastQuarter4
 }
